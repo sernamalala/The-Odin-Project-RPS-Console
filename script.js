4f046e83385f09568835899e4d40e1a8d19c8d8d
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 05:17:09 +0200

    Made the computer choice function using Math.random

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello Serna");
+
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+console.log(getComputerChoice());
